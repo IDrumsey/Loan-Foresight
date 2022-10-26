98f35604b1a376bdba3ca357d3bd2551c73b0505
IDrumsey <claydevelop@gmail.com>
2022-10-26 19:20:18 -0400

    Set up Mantine

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { MantineProvider } from '@mantine/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* https://mantine.dev/guides/next/ */}
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark'
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
 }
 
 export default MyApp
