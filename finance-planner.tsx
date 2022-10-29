2c03253406b29177a40296187d8ec6caf28f5a9b
IDrumsey <claydevelop@gmail.com>
2022-10-28 21:04:47 -0400

    Added calcProjectedNets

---
+++ finance-planner.tsx
@@ -0,0 +1,54 @@
+
+
+export const calcProjectedNets = (baseNet: number, annualSalary: number, expectedAnnualSalaryIncrease: number, numMonthsToProject = 12, granularity: 'months' | 'years') => {
+    // https://stackoverflow.com/a/23072573/17712310
+    // https://tsdoc.org/
+    /**
+     * @param granularity - Over what time period to split calculations
+     * @param annualSalary - The expected annual salary
+     * @param expectedAnnualSalaryIncrease - The expected annual salary increase
+     * 
+     * @returns - array of dates and the expected salary
+     */
+
+    let maxTimeIncrementNum = numMonthsToProject
+
+    if(granularity == 'years'){
+        maxTimeIncrementNum = numMonthsToProject / 12
+    }
+
+    let expectedNetWorth = []
+
+    let currWorth = baseNet || 0
+    let expectedSalary = annualSalary
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+    for(let i = 1; i <= maxTimeIncrementNum; i++) {
+        let nodeDate = new Date()
+
+        // add time
+        switch(granularity) {
+            case 'years': {
+                // https://codingbeautydev.com/blog/javascript-add-years-to-date/
+                nodeDate.setFullYear(nodeDate.getFullYear() + i)
+                break
+            }
+            case 'months': {
+                nodeDate.setMonth(nodeDate.getMonth() + i)
+                break
+            }
+        }
+        
+        currWorth = currWorth += granularity == 'years' ? expectedSalary : expectedSalary / 12
+
+        
+        expectedNetWorth.push({
+            date: nodeDate,
+            expected: currWorth
+        })
+
+        expectedSalary += expectedSalary * expectedAnnualSalaryIncrease
+    }
+
+    return expectedNetWorth
+}
