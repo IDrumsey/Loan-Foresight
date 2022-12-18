1a231eb85e3e3b726896bd37b62a6e18d52be4db
IDrumsey <claydevelop@gmail.com>
2022-12-18 16:30:54 -0500

    Potential production bug fix.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // https://github.com/chartjs/Chart.js/issues/9390#issuecomment-1263680623
+  // https://stackoverflow.com/questions/73840076/error-rangeerror-minimumfractiondigits-value-is-out-of-range-with-chartjs-in?noredirect=1&lq=1
+  // https://github.com/chartjs/Chart.js/issues/10673
+  swcMinify: false,
 }
 
 module.exports = nextConfig
